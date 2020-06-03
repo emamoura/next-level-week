924b639c848741647cb17771f7a8f514def8368b
emamoura <emanuel.moura@ccc.ufcg.edu.br>
2020-06-03 17:30:34 -0300

    Dia 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,11 +24,15 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( (res) => { return res.json() })
     .then( cities => {
+        
         for(const city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
  
         citySelect.disabled = false
@@ -38,3 +42,40 @@ function getCities(event) {
 document
     .querySelector("select[name = uf]")
     .addEventListener("change", getCities)
+
+const itemsToCollect  = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target 
+
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+
+    const alreadySelected = selectItems.findIndex( item => {
+        const itemFound = item === itemdId
+        return itemFound
+    })
+
+    if(alreadySelected >= 0) {
+        const filteredItems = selectItems.filter(item => {
+            itemIsDifferente = item != itemId
+            return itemIsDifferente
+        })
+
+
+        selectItems = filteredItems
+    } else {
+        selectItems.push(itemId)
+    }
+
+    collectedItems.value = selectItems
+}
